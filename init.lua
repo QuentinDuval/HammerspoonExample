53f61e98d4fd305032b5c8eeb57103616e143a3d
QuentinDuval <senmenty@gmail.com>
2021-02-06 15:00:06 -0500

    First experiments searching the tabs in google

--- init.lua
+++ init.lua
@@ -12,6 +12,9 @@ require "search";
 -- ****************************************************
 
 
+local log = hs.logger.new('mymodule','debug')
+
+
 function on_omni_search()
     local content = copy_selected_text()
     omni_search(content)
@@ -60,6 +63,118 @@ end)
 
 hs.hotkey.bind({"cmd", "alt"}, "z", function()
     text_expander()
+end)
+
+
+-- *****************************
+-- Kind of text expander
+-- *****************************
+
+
+-- Taken from https://liuhao.im/english/2017/06/02/macos-automation-and-shortcuts-with-hammerspoon.html#id-open-a-certain-chrome-tab-with-alt--
+function chrome_active_tab_with_name(name)
+    return function()
+        hs.osascript.javascript([[
+            // below is javascript code
+            var chrome = Application('Google Chrome');
+            chrome.activate();
+            var wins = chrome.windows;
+
+            // loop tabs to find a web page with a title of <name>
+            function main() {
+                for (var i = 0; i < wins.length; i++) {
+                    var win = wins.at(i);
+                    var tabs = win.tabs;
+                    for (var j = 0; j < tabs.length; j++) {
+                    var tab = tabs.at(j);
+                    tab.title(); j;
+                    if (tab.title().indexOf(']] .. name .. [[') > -1) {
+                            win.activeTabIndex = j + 1;
+                            return;
+                        }
+                    }
+                }
+            }
+            main();
+            // end of javascript
+        ]])
+    end
+end
+
+
+function get_sub_item(items, name)
+    for i, item in ipairs(items) do
+        if item.AXTitle == name then
+            return item
+        end
+    end
+    return nil
+end
+
+
+function search_children(items, search)
+    for i, list in ipairs(items) do
+        for i, item in ipairs(list) do
+            log:d(hs.inspect(item))
+            if string.find(item.AXTitle, search) then
+                return item
+            end
+        end
+    end
+    return nil
+end
+
+
+hs.hotkey.bind({"cmd", "alt"}, "x", function()
+
+    -- open to focus on the window instead
+    local ap = hs.application.find("Google Chrome")
+    -- hs.alert(ap:name())
+    -- hs.alert(ap:path())
+
+    ap:activate(true)
+    local w = ap:focusedWindow()
+    -- local w = ap:findWindow("hammerspoon")
+    -- hs.alert(w:title()) -- works
+    -- hs.alert(w:isFullScreen()) -- works
+    -- hs.alert(w:tabCount()) -- does not work for chrome...
+    -- chrome_active_tab_with_name("Kilian")
+
+    -- Not really working with Chrome: only one window returned, the current one
+    -- for i, w in ipairs(ap:allWindows()) do
+    --     hs.alert(w:title())
+    -- end
+
+    -- You can find a tab based on a regex search (but select first match)
+    -- Typically, it will go into the "recently closed" first, which is not what I want
+    -- local searched = "Boîte de réception.*Gmail.*"
+    --[[
+    local searched = "Watch.*"
+    local item = ap:findMenuItem(searched, true)
+    log:d(hs.inspect(item))
+    hs.alert(item)
+    if (item) then
+        ap:selectMenuItem(searched, true)
+    else
+        ap:selectMenuItem("Nouvel onglet")
+        hs.eventtap.keyStrokes("gmail.com")
+        hs.eventtap.keyStroke({}, "return") -- validate auto completion
+        hs.eventtap.keyStroke({}, "return") -- run the search
+    end
+    ]]--
+    
+    local items = ap:getMenuItems()
+    items = get_sub_item(items, "Onglet")
+    item = search_children(items.AXChildren, "Gmail")
+    if (item) then
+        ap:selectMenuItem({"Onglet", item.AXTitle})
+    else
+        ap:selectMenuItem("Nouvel onglet")
+        -- ap:selectMenuItem({"Fichier", "Nouvel onglet"})
+        hs.eventtap.keyStrokes("gmail.com")
+        hs.eventtap.keyStroke({}, "return") -- validate auto completion
+        hs.eventtap.keyStroke({}, "return") -- run the search
+    end  
 end)
 
 
