952b67290a3a4579e1e56901e74404256aebf763
Quentin Duval <senmenty@gmail.com>
2021-10-24 16:47:14 -0400

    ideas

--- core/expander.lua
+++ core/expander.lua
@@ -71,6 +71,29 @@ function Expander:show(on_hide)
         self:apply_rule(choice.text)
     end)
 
+    --[[
+    t = require("hs.webview.toolbar")
+    a = t.new("myConsole", {
+            { id = "select1", selectable = true, image = hs.image.imageFromName("NSStatusAvailable") },
+            { id = "NSToolbarSpaceItem" },
+            { id = "select2", selectable = true, image = hs.image.imageFromName("NSStatusUnavailable") },
+            { id = "notShown", default = false, image = hs.image.imageFromName("NSBonjour") },
+            { id = "NSToolbarFlexibleSpaceItem" },
+            { id = "navGroup", label = "Navigation", groupMembers = { "navLeft", "navRight" }},
+            { id = "navLeft", image = hs.image.imageFromName("NSGoLeftTemplate"), allowedAlone = false },
+            { id = "navRight", image = hs.image.imageFromName("NSGoRightTemplate"), allowedAlone = false },
+            { id = "NSToolbarFlexibleSpaceItem" },
+            { id = "cust", label = "customize", fn = function(t, w, i) t:customizePanel() end, image = hs.image.imageFromName("NSAdvanced") }
+        }):canCustomize(true)
+          :autosaves(true)
+          :selectedItem("select2")
+          :setCallback(function(...)
+                            print("a", inspect(table.pack(...)))
+                       end)
+    
+    chooser:attachedToolbar(a)
+    ]]
+
     -- Searching for all query parts where query
     -- parts are separated with the '+' sign
     chooser:queryChangedCallback(function(query)
