cac2d9463cb67ead625c9e3bce62c9a0b9f1c3ba
QuentinDuval <senmenty@gmail.com>
2022-07-18 20:31:15 -0400

    Jekyll helpers

--- project/code/jekyll.lua
+++ project/code/jekyll.lua
@@ -17,5 +17,22 @@ jekyll_lua_code:register_all{
             use_math: true
             ---
         ]]
+    },
+    {
+        text="Link",
+        content=[[({% link _posts/2017-07-06-hexagonal-architecture-free-monad.md %})]]
+    },
+    {
+        text="Code block",
+        fct=function()
+            local raw_copied_text = string.strip(get_selected_text())
+            local content = string.dedent[[
+                ```haskell
+                %s
+                ```
+            ]]
+            content = string.format(content, raw_copied_text)
+            write_lines(content)
+        end
     }
 }
